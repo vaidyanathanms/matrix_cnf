30e9910c9c5e97eedb9d74b700480f7e627ad09b
vaidyanathanms <v0e@ornl.gov>
2022-09-19 18:25:19 -0400

    major commit - combined Shalini's and my code for both cell and poly

--- cg_martini/src/src_py/gen_top.py
+++ cg_martini/src/src_py/gen_top.py
@@ -1,17 +1,40 @@
 # To generate topology for cellulose/matrices
-
+# Combined both cellulose and matrix in one code.
+# Version 2.0: Sept-19-2022
+# Authors: Shalini Jayaraman Rukmani & Vaidyanathan Sethuraman
+import os
+import sys
+import numpy
+import re
+import shutil
+import glob
+import math
+import subprocess
 from auxgen_top import *
+from auxgen_params import *
 
-#----Read input file - filename, ncnf_fibers, acetfrac--------------
+#----Read input file - matrix/filename,ncnf_fibers,acetfrac-----------
 ch_per_cnf   = 18 # default
-if len(sys.argv) == 6:
+if len(sys.argv) == 4: #polymer matrix
+    print('Generating CG polymer for ', sys.argv[1])
+    if sys.argv[1] == 'petg':
+        design_petg(int(sys.argv[2]),sys.argv[1].upper())
+    elif sys.argv[1] == 'pla':
+        design_pla(int(sys.argv[2]),sys.argv[1].upper())
+    elif sys.argv[1] == 'p3hb':
+        design_p3hb(int(sys.argv[2]),sys.argv[1].upper())
+    else:
+        raise RuntimeError('Unknown matrix input: ' + sys.argv[1])
+    exit()
+elif len(sys.argv) == 6: # Cellulose
     ch_per_cnf = int(sys.argv[5]) # number of chains per bundle
 elif len(sys.argv) != 5: 
     print('Unknown number of arguments: ', len(sys.argv),\
           str(sys.argv))
     exit()
 
-print('Input file name: ',sys.argv[1])
+#---------------- Analyzing cellulose -----------------------------
+print('Input file name for cellulose CG: ',sys.argv[1])
 
 #-----Process input data - Cellulose/Acetylated Cellulose-----------
 fname    = str(sys.argv[1]) # Coarse-grained file
@@ -30,13 +53,12 @@ residarr,resnamearr,aidarr,anamearr,rxarr,ryarr,rzarr,massarr = read_gro_file(fn
 #-----Generate bead list--------------------------------------------
 glycan_list = create_martini_beads(cell_dp,ncnf,ch_per_cnf,residarr,\
                                    aidarr,anamearr)
-# CREATE BOND LIST
+# Create bond list
 bond_list = create_bond_list(cell_dp,ncnf,ch_per_cnf,glycan_list)
-
-# CREATE ANGLE LIST
-# CREATE DIHEDRAL LIST
-# CREATE ATOMTYPE LIST
-# CREATE NONBONDED PARAMETER LIST
-# CREATE BONDED PARAMETER LIST
+# Create angle list
+angle_list = create_angle_list(cell_dp,ncnf,ch_per_cnf,glycan_list)
+# Create dihedral list
+dihed_list = create_dihedral_list(cell_dp,ncnf,ch_per_cnf,glycan_list)
 # COMBINE AND WRITE
-
+ftop = write_top(residarr,resnamearr,aidarr,anamearr,massarr,bond_list,angle_list,dihed_list)
+# NOTE: The parameters for cellulose are added in cell_martini3.itp
