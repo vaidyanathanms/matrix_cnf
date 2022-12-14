ba4117273e69745307cc0f6c9e080ed172822a66
vaidyanathanms <v0e@ornl.gov>
2022-12-14 11:23:43 -0500

    changed path

--- cnf_atomistic/pack_files/dir_names.py
+++ cnf_atomistic/pack_files/dir_names.py
@@ -1,12 +1,12 @@
 # User needs to input data for all this case
 # Input/Output directory paths
 
-acet_dir   = '/home/v0e/allcodes/files_cnf/main/acetylation_files' # input dir for acetylation
-cell_topdir= '/home/v0e/allcodes/files_cnf/main/cell_toppar' # top/par dir for cellulose/acetylated cell
-natv_cnfdir= '/home/v0e/allcodes/files_cnf/main/elementary_fibrils' #cellulose inps
+acet_dir   = '/home/v0e/allcodes/files_cnf/cnf_atomistic/acetylation_files' # input dir for acetylation
+cell_topdir= '/home/v0e/allcodes/files_cnf/cnf_atomistic/cell_toppar' # top/par dir for cellulose/acetylated cell
+natv_cnfdir= '/home/v0e/allcodes/files_cnf/cnf_atomistic/elementary_fibrils' #cellulose inps
 poly_mat   = '/home/v0e/allcodes/files_cnf/polymer_matrices' #i/o dir poly matrices
 pack_exe   = '/home/v0e/packmol/packmol' # packmol executable
-mdp_dir    = '/home/v0e/allcodes/files_cnf/main/mdp_files' # mdp files for GMX
+mdp_dir    = '/home/v0e/allcodes/files_cnf/cnf_atomistic/mdp_files' # mdp files for GMX
 scr_dir    = '/lustre/or-scratch/cades-bsd/v0e' # scratch dir (MD run dir)
 #------------------------------------------------------------------
 # NOTE: Directory from CHARMM should be inside "poly_mat" (see above)# directory with the prefix charmm_
